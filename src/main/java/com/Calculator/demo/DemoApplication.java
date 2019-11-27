536996fee6ece32a2fafe95970443be85650d27c
[LastFennek] <[scherzma@gmail.com]>
2019-11-27 14:09:07 +0100

    Funktion

--- src/main/java/com/Calculator/demo/DemoApplication.java
+++ src/main/java/com/Calculator/demo/DemoApplication.java
@@ -3,11 +3,20 @@ package com.Calculator.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bitte geben Sie Zahl eins ein:");
+		double x = scan.nextInt();
+		System.out.println("Bitte geben Sie Zahl zwei ein:");
+		double sum = x + scan.nextInt();
+		System.out.println("Summe / Produkt= "+  + sum/((sum-x)*x));
 	}
 
 }
